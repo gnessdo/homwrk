72c5b7e54a005c6ae99a55b16e8753b4e0aa63a1
gnessdo <87198355+gnessdo@users.noreply.github.com>
2021-09-14 18:19:01 +0400

    Add files via upload

---
+++ 1/script.js
@@ -0,0 +1,28 @@
+let mail = "gnessdo@gmail.com";
+let pass = "wott92e1";
+
+
+function test () {
+    let x = document.getElementById ("inputEmail3").value;
+    let y = document.getElementById ("inputPassword3").value;
+    let span = document.getElementById ("span");
+}
+
+if(x==mail) {
+    alert ("you entered right email");
+
+}
+
+else {
+    alert ("your email is invalid. Try again");
+}
+
+if else (y==pass) {
+    alert ("wrong password");
+}
+
+else {
+    alert ("correct password");
+}
+
+test();
